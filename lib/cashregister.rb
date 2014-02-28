d203fbf015f08df42c6d6ea9e0603e8e55e662db
Andrew Dixon <andrew.dixon@twitch.tv>
2014-02-28 15:19:42 -0800

    Finished CashRegister class and specs

--- lib/cashregister.rb
+++ lib/cashregister.rb
@@ -1,2 +1,43 @@
-class Cashregister
+require 'change'
+
+class CashRegister
+  attr_accessor :coins
+
+  def coins=(coins=[25,10,5,1])
+    raise ArgumentError, "Expected an Array, got #{coins.class.name}" unless coins.is_a? Array
+    raise ArgumentError, 'Did not receive an Array of Integers' unless coins.all? { |coin| coin.is_a? Integer }
+
+    @optimal_change = Hash.new { |hash, key| hash[key] = get_optimal_change(key) }
+    @coins = coins
+  end
+
+  alias :initialize :coins=
+
+    def make_change(amount)
+      raise ArgumentError, "Expected Integer, got #{amount.class.name}" unless amount.is_a? Integer
+
+      return @optimal_change[amount]
+    end
+
+  private
+
+  def get_optimal_change(amount)
+    if amount < @coins.min
+      nil
+    elsif @coins.include?(amount)
+      Change.new(@coins).add(amount)
+    else
+      @coins.sort.reverse.select do |coin|
+        coin < amount
+      end.reduce([]) do |coins, coin|
+        coins.all? { |larger_coin| larger_coin % coin != 0 } ? coins << coin : coins
+      end.map do |coin|
+        @optimal_change[amount - coin].add(coin) unless @optimal_change[amount - coin].nil?
+      end.compact.select do |change|
+        change.value == amount
+      end.min do |a,b|
+        a.count_coins <=> b.count_coins
+      end
+    end
+  end
 end
--- spec/cashregister_spec.rb
+++ spec/cashregister_spec.rb
@@ -1,12 +1,40 @@
 require 'spec_helper'
 require 'cashregister'
+require 'its'
 
-describe Cashregister do
+describe CashRegister do
+  subject(:cash_register) { CashRegister.new(coins) }
+  let(:coins) { [25,10,5,1] }
+
   describe "#initialize" do
-    it "instantiates" do
-      expect {
-        Cashregister.new
-      }.to_not raise_exception
+    context 'given [25,10,5,1]' do
+      specify { expect { cash_register }.to_not raise_exception }
+    end
+    context 'when not given an Array' do
+      let(:coins) { 'quarters and nickles' }
+      specify { expect { cash_register }.to raise_exception }
+    end
+    context 'when not given an Array of Integers' do
+      let(:coins) { ['quarters', 'nickles'] }
+      specify { expect { cash_register }.to raise_exception }
+    end
+  end
+
+  describe '.make_change' do
+    its(:make_change, 0)   { should eq(nil) }
+    its(:make_change, 5)   { should eq({25 => 0, 10 => 0, 5 => 1, 1 => 0}) }
+    its(:make_change, 25)  { should eq({25 => 1, 10 => 0, 5 => 0, 1 => 0}) }
+    its(:make_change, 123) { should eq({25 => 4, 10 => 2, 5 => 0, 1 => 3}) }
+    context 'when calculating with crazy foreign coins' do
+      let(:coins) { [10,7,1] }
+      its(:make_change, 14) { should eq({10 => 0, 7 => 2, 1 => 0}) }
+    end
+    context 'when given a complex problem' do
+      let(:coins) { [2,5,12,18,35,36] }
+      its(:make_change, 67) { should eq({2=>1, 5=>0, 12=>1, 18=>1, 35=>1, 36=>0}) }
+    end
+    context 'when given a bad argument' do
+      specify { expect { cash_register.make_change('about tree-fiddy') }.to raise_exception }
     end
   end
 end
